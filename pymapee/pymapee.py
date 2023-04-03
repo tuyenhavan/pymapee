bfd9e53a79db455e6698c30974e59796f32870ab
tuyenhavan <tuyenmassey@gmail.com>
2023-04-03 20:00:26 +0200

    updated files

--- pymapee/pymapee.py
+++ pymapee/pymapee.py
@@ -1,79 +1,165 @@
 """Main module."""
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
+import ee
+from utils import (date_range_col, monthly_datetime_list,
+                    cloud_mask, scaling_data)
+
+def modis_cloud_mask(col, from_bit, to_bit, QA_band="DetailedQA", threshold=1):
+    """ Return a collection of MODIS cloud-free images
+
+        Args:
+            col (ee.ImageCollection): The input image collection.
+            from_bit (int): The start bit to extract.
+            to_bit (int): The last bit to extract.
+            QA_band (str|optional): The quality band which contains cloud-related infor. Default to DetailedQA.
+            threshold (int|optional): The threshold value to mask cloud-related pixels. Default to 1.
+
+        Returns:
+            ee.ImageCollection: The output collection with cloud-free pixels.
+    """
+    if not isinstance(col, ee.ImageCollection):
+        raise TypeError ("Unsupported data type. It only supports ee.ImageCollection")
+    out_col=cloud_mask(col, from_bit, to_bit, QA_band, threshold)
+    return out_col
+
+def landsat_cloud_mask(col, from_bit, to_bit, QA_band="QA_PIXEL ", threshold=1):
+    """ Return a collection of Landsat cloud-free images
+
+        Args:
+            col (ee.ImageCollection): The input image collection.
+            from_bit (int): The start bit to extract.
+            to_bit (int): The last bit to extract.
+            QA_band (str|optional): The quality band which contains cloud-related infor. Default to QA_PIXEL.
+            threshold (int|optional): The threshold value to mask cloud-related pixels. Default to 1.
+
+        Returns:
+            ee.ImageCollection: The output collection with cloud-free pixels.
+    """
+    if not isinstance(col, ee.ImageCollection):
+        raise TypeError ("Unsupported data type. It only supports ee.ImageCollection")
+    out_col=cloud_mask(col, from_bit, to_bit, QA_band, threshold)
+    return out_col
+
+def sentinel2_cloud_mask(col, from_bit, to_bit, QA_band="QA60", threshold=0):
+    """ Return a collection of Sentinel-2 cloud-free images
+
+        Args:
+            col (ee.ImageCollection): The input image collection.
+            from_bit (int): The start bit to extract (e.g., 10)
+            to_bit (int): The last bit to extract (e.g., 11)
+            QA_band (str|optional): The quality band which contains cloud-related infor. Default to DetailedQA.
+            threshold (int|optional): The threshold value to mask cloud-related pixels. Default to 1.
+
+        Returns:
+            ee.ImageCollection: The output collection with cloud-free pixels.
+    """
+    if not isinstance(col, ee.ImageCollection):
+        raise TypeError ("Unsupported data type. It only supports ee.ImageCollection")
+    out_col=cloud_mask(col, from_bit, to_bit, QA_band, threshold)
+    return out_col
+
+
+def monthly_composite(col, mode=None):
+    """ Return a collection of monthly images
+
+        Args:
+            col (ee.ImageCollection): The input image collection.
+            mode (str): The aggregated method. Supported modes 'max', 'min',
+                        'median', 'mean', 'sum'. Default to None.
+
+        Returns:
+            ee.ImageCollection: A output image collection of monthly images.
+   """
+    if  not isinstance(col, ee.ImageCollection):
+        raise TypeError ("Unsupported data type. Expected data is ee.ImageCollection")
+    if not isinstance(mode, (str, type(None))):
+        raise TypeError ("Unsupported data type. Mode should be string")
+    if mode is None:
+        mode="max"
+    mode=mode.lower().strip()
+    if mode not in ["max", "mean", "median", "mvc", "min", "sum"]:
+        raise ValueError ("Unsupported methods. Please choose mean, max, min, sum, or median")
+
+    first_date, latest_date=date_range_col(col)
+    monthly_list=monthly_datetime_list(first_date, latest_date)
+    def monthly_data(date):
+        start_date=ee.Date(date)
+        end_date=start_date.advance(1,"month")
+        monthly_col=col.filterDate(start_date, end_date)
+        size=monthly_col.size()
+
+        if mode == "mean":
+            img=monthly_col.mean().set({"system:time_start":start_date.millis()})
+        elif mode == "max":
+            img=monthly_col.max().set({"system:time_start":start_date.millis()})
+        elif mode == "min":
+            img=monthly_col.min().set({"system:time_start":start_date.millis()})
+        elif mode in ["median", "mvc"]:
+            img=monthly_col.median().set({"system:time_start":start_date.millis()})
+        else:
+            img=monthly_col.sum().set({"system:time_start":start_date.millis()})
+        return ee.Algorithms.If(size.gt(0), img)
+    composite_col = ee.ImageCollection.fromImages(monthly_list.map(monthly_data))
+    return composite_col
+
+def VAI(col, scale=1):
+    """ Return a collection of monthly vegetation anomaly index.
+
+        Args:
+            col (ee.ImageCollection): The input image collection.
+            scale (int|float|optional): Scaling factor
+
+        Returns:
+            ee.ImageCollection: The output collection with vegetation Anomaly Index (VAI).
+    """
+    if not isinstance(col, ee.ImageCollection):
+        raise TypeError ("Unsupported data type. Please provide ee.ImageCollection.")
+    col=scaling_data(col, scale)
+
+    first_date, latest_date=date_range_col(col)
+    monthly_list=monthly_datetime_list(first_date, latest_date)
+    def ndvi_anomaly(date):
+        start_time=ee.Date(date)
+        set_month=ee.Number.parse(start_time.format("MM"))
+        last_time=start_time.advance(1,"month")
+        col_month=col.filter(ee.Filter.calendarRange(set_month,set_month,"month"))
+        subcol=col.filterDate(start_time,last_time)
+        size=col_month.size()
+        mean=col_month.mean()
+        anomaly=subcol.max().subtract(mean).set({"system:time_start":start_time.millis()})
+        return ee.Algorithms.If(size.gt(0), anomaly.rename("VAI"))
+    vai=ee.ImageCollection.fromImages(monthly_list.map(ndvi_anomaly))
+    return vai
+
+def VCI(col):
+    """ Return a collection of vegetation condition index.
+
+        Args:
+            col (ee.ImageCollection): The input image collection.
+            scale (int|float|optional): Scaling factor
+
+        Returns:
+            ee.ImageCollection: The output collection with vegetation Anomaly Index (VAI).
+    """
+    if not isinstance(col, ee.ImageCollection):
+        raise TypeError ("Unsupported data type. Please provide ee.ImageCollection.")
+
+    first_date, latest_date=date_range_col(col)
+    monthly_list=monthly_datetime_list(first_date, latest_date)
+    def vci(date):
+        start_time=ee.Date(date)
+        set_month=ee.Number.parse(start_time.format("MM"))
+        last_time=start_time.advance(1,"month")
+        col_month=col.filter(ee.Filter.calendarRange(set_month,set_month,"month"))
+        subcol=col.filterDate(start_time,last_time)
+        size=col_month.size()
+        mean=col_month.mean()
+        min_value=col_month.min()
+        max_value=col_month.max()
+        vci_img=subcol.max().subtract(min_value).divide(max_value.subtract(min_value)).multiply(100)
+        vci_img=vci_img.set({"system:time_start":start_time.millis()}).rename("VCI")
+        return ee.Algorithms.If(size.gt(0), vci_img)
+    vci_col=ee.ImageCollection.fromImages(monthly_list.map(vci))
+    return vci_col
 
 
 
@@ -81,7 +167,9 @@
 
 def download_ee(ds,aoi,folder_name="GEE_Data",res=1000):
     """ Export an image from GEE with a given scale and area of interest
-    to the Google Drive.
+    to the Google Drive. If input data is an ImageCollection, it will convert it
+    into an image and then export. The collection should contains only single data,
+    for example NDVI bands or precipitation bands or LST bands.
 
         Args:
             ds (ee.Image|ee.ImageCollection): The input ee.Image or ee.ImageCollection
--- pymapee/utils.py
+++ pymapee/utils.py
@@ -19,7 +19,7 @@ def bitwise_extract(img, from_bit, to_bit):
     """
     mask_size=ee.Number(to_bit).add(ee.Number(1)).subtract(from_bit)
     mask=ee.Number(1).leftShift(mask_size).subtract(1)
-    out_img=image.rightShift(from_bit).bitwiseAnd(mask)
+    out_img=img.rightShift(from_bit).bitwiseAnd(mask)
     return out_img
 
 def cloud_mask(col, from_bit, to_bit, QA_band, threshold=1):
@@ -44,30 +44,8 @@ def cloud_mask(col, from_bit, to_bit, QA_band, threshold=1):
     cloudless_col=col.map(img_mask)
     return cloudless_col
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
 ##############################################################################
-#                                 Other Untilities                           #
+#                            Datetime Untilities                             #
 ##############################################################################
 
 def time_convert(date_code):
@@ -87,3 +65,84 @@ def time_convert(date_code):
     delta=datetime.timedelta(hours=hour_number)
     end_date=start_date+delta
     return end_date
+
+def date_range_col(col):
+    """ Return the first and latest datetimes of image acquision in the collection
+
+        Args:
+            col (ee.ImageCollection): The input image collection.
+
+        Returns:
+            tuple: The ee.Date object
+    """
+    first_date=ee.Date(col.first().get("system:time_start"))
+    latest_date=ee.Date(col.limit(1, "system:time_start", False).first().get("system:time_start"))
+    return first_date, latest_date
+
+def monthly_datetime_list(first_date, latest_date):
+    """ Return a list of monthly datetime objects.
+
+        Args:
+            first_date(ee.date): The first date of collection.
+            latest_date(ee.Date): The latest date of collection.
+
+        Returns:
+            ee.List: The list of monthly datetime objects.
+    """
+    m=ee.Number.parse(first_date.format("MM"))
+    y=ee.Number.parse(first_date.format("YYYY"))
+    month_count=latest_date.difference(first_date,"month").round()
+    month_list=ee.List.sequence(0, month_count)
+    def month_step(month):
+        first_month=ee.Date.fromYMD(y,m,1)
+        next_month=first_month.advance(month,"month")
+        return next_month.millis()
+    monthly_list=month_list.map(month_step)
+    return monthly_list
+
+##############################################################################
+#                                 Other Untilities                           #
+##############################################################################
+
+
+def scaling_data(col, scale_factor=None):
+    """ Scale value of an image or collection
+
+        Args:
+            col (ee.ImageCollection|ee.Image): The input image or collection.
+            scale_factor (int|float|optional): The scale given by scale information in image band. Default to None.
+
+        Returns:
+            ee.Image|ee.ImageCollection: The output image or collection with scaled values.
+
+    """
+    if scale_factor is None:
+        scale_factor=1
+    if isinstance(col, ee.Image):
+        out_data=ee.Image(col.multiply(scale_factor).copyProperties(col, col.propertyNames()))
+    if isinstance(col, ee.ImageCollection):
+        out_data=col.map(lambda img: img.multiply(scale_factor).copyProperties(img, img.propertyNames()))
+    return out_data
+
+def kelvin_celsius(col):
+    """ Convert temperature from Kelvin unit to celsius degree
+
+        Args:
+            col (ee.Image|ee.ImageCollection): The input image or collection for unit conversion.
+
+        Returns:
+            ee.Image|ee.ImageCollection: The converted output image or collection in celsius unit.
+    """
+    if isinstance(col, ee.Image):
+        out_data=ee.Image(col.subtract(273.15).copyProperties(col, col.propertyNames()))
+    if isinstance (col, ee.ImageCollection):
+        out_data=col.map(lambda img: img.subtract(273.15).copyProperties(img, img.propertyNames()))
+    return out_data
+
+def col_timestamp_band(col):
+    """ return a collection with a new band added called timestamp"""
+    def time_band(img):
+        _time_band=img.metadata("system:time_start").rename("timestamp")
+        time_band_mask=_time_band.updateMask(img.mask().select(0))
+        return img.addBands(time_band_mask)
+    return col.map(time_band)
